878a0ef2a31086d9005ce89f1fed21eeebd1f21b
jayashree <sjayashree529@gmail.com>
2022-04-28 16:20:41 +0530

    checked

---
+++ src/com/bridgelabz/AddTwoNumber.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class AddTwoNumber {
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
+
+	public static void main(String[] args) {
+		add(10,20);
+
+	}
+
+}
